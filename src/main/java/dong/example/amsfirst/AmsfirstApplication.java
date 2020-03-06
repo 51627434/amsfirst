f9edb8f88a86cc41019f98ca30454cda5cb60fcb
dong <dongxueqiang008@qq.com>
2020-03-06 21:27:15 +0800

    0.0.1 创建基础框架，加入axis2依赖，加入CSPResponseService.wsdl services.xml

---
+++ src/main/java/dong/example/amsfirst/AmsfirstApplication.java
@@ -0,0 +1,13 @@
+package dong.example.amsfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dong/example/amsfirst/AmsfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package dong.example.amsfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
